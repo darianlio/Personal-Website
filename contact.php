25cc4983ae2343305451127ddb9df0461fc68c70
Darian <14dl50@queensu.ca>
2017-09-21 01:54:13 -0400

    Attempting php contact form

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Me";
+$formcontent=" From: $name \n Message: $message";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>

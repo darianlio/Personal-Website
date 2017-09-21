cb72debdd44f999c397ec4847ef2d5fa570c3fb5
Darian <14dl50@queensu.ca>
2017-09-21 00:53:23 -0400

    added smooth scrolling using jquery

--- js/javascript.js
+++ js/javascript.js
@@ -1,32 +1,14 @@
-(function( $ ){
-
-  $.fn.fitText = function( kompressor, options ) {
-
-    // Setup options
-    var compressor = kompressor || 1,
-        settings = $.extend({
-          'minFontSize' : Number.NEGATIVE_INFINITY,
-          'maxFontSize' : Number.POSITIVE_INFINITY
-        }, options);
-
-    return this.each(function(){
-
-      // Store the object
-      var $this = $(this);
-
-      // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
-
-      // Call once to set.
-      resizer();
-
-      // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize.fittext orientationchange.fittext', resizer);
-
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function(e){
+        e.preventDefault();
+        
+        var target = this.hash;
+        var $target = $(target);
+        
+        $('html, body').animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing', function(){
+            window.location.hash = target;
+        });
     });
-
-  };
-
-})( jQuery );
+});

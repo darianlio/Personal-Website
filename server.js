006acdb25dd6e987d0b0c01984dd18987b66ea20
Darian Lio <darianlio97@gmail.com>
2018-07-19 15:20:11 -0400

    deployed to website

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/', router);
 //set static files (css js and images) locations
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(8080, function(){
+var server = app.listen(80, function(){
 	var host = server.address().address
 	var port = server.address().port
    	console.log("Example app listening at http://%s:%s", host, port)
